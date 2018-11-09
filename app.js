6707ac4ca11d5664747d7d997170dcd0d44a9593
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2018-11-09 16:36:23 -0500

    user and wiki added

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 
+//Routers
+// const user = require('./routes/user.js');
+// const wiki = require('./routes/wiki.js');
+
+// const { addPage } = require('./views');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
+// app.use('/wiki', wiki);
+// app.use('User', user);
 
 //PORT configuration.
 
@@ -23,8 +31,9 @@ const models = require('./models');
 
 const init = async () => {
   try {
-    await models.User.sync({force: true});
-    await models.Page.sync({force: true});
+    // await models.User.sync({force: true});
+    // await models.Page.sync({force: true});
+    await models.db.sync({force: true});
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}.`);
     });
@@ -42,3 +51,15 @@ init();
 app.use('/', (req, res, next) => {
   res.send('<h1>Welcome to the root.</h1>');
 });
+
+// router.get('/', (req, res, next) => {
+//   res.send('got to GET /wiki/');
+// });
+
+// router.send('/',(req, res, next) => {
+//   res.send('got to POST /wiki/');
+// });
+
+// router.get('/add', (req, res, next) => {
+//   res.send('got to GET /wiki/add');
+// });
